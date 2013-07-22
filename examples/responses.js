2c8c99615eeed405d4332efd90f3c17ca158b5bd
Euge Gimelberg <eugene@rightscale.com>
2013-07-22 11:10:12 -0700

    canned-responses are now just simply defined as a module that is required

--- examples/responses.js
+++ examples/responses.js
@@ -1,21 +1,23 @@
-get("/name", function(req, res) {
-  res.send({
-    name : "bob",
-    age : 23
+module.exports = function(server) {
+  server.get("/name", function(req, res) {
+    res.send({
+      name : "bob",
+      age : 23
+    });
   });
-});
 
-get("/stuff", function(req, res) {
-  res.send({
-    name : "hahah",
-    address : "1234 sjdasd"
+  server.get("/stuff", function(req, res) {
+    res.send({
+      name : "hahah",
+      address : "1234 sjdasd"
+    });
   });
-});
 
-get("/monkey", function(req, res) {
-  res.set({
-    'X-Monkey-Header': '12345'
+  server.get("/monkey", function(req, res) {
+    res.set({
+      'X-Monkey-Header': '12345'
+    });
+    res.status(323);
+    res.send({ value : Math.random(10) });
   });
-  res.status(323);
-  res.send({ value : Math.random(10) });
-});
+};
--- test/fixtures/responses.js
+++ test/fixtures/responses.js
@@ -1,30 +1,33 @@
-get("/people", function(req, res) {
-  res.send([
-    { name : "Euge" },
-    { name : "Bob" } ]);
-});
+module.exports = function(server) {
 
-get("/people/1", function(req, res) {
-  res.send({
-    name : "Euge",
-    address : "1234 Main St."
+  server.get("/people", function(req, res) {
+    res.send([
+      { name : "Euge" },
+      { name : "Bob" } ]);
   });
-});
 
-post("/people", function(req, res) {
-  res.send({
-    name : "Bob",
-    id : 10
+  server.get("/people/1", function(req, res) {
+    res.send({
+      name : "Euge",
+      address : "1234 Main St."
+    });
   });
-});
 
-put("/people/10", function(req, res) {
-  res.send({
-    name : "NotBob",
-    id : 10
+  server.post("/people", function(req, res) {
+    res.send({
+      name : "Bob",
+      id : 10
+    });
   });
-});
 
-del("/people/10", function(req, res) {
-  res.send({});
-});
+  server.put("/people/10", function(req, res) {
+    res.send({
+      name : "NotBob",
+      id : 10
+    });
+  });
+
+  server.del("/people/10", function(req, res) {
+    res.send({});
+  });
+};
